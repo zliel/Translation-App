837ea4140acd665ede10f1a25bc3996711c26a7b
zliel <zpliel@gmail.com>
2022-07-15 01:23:56 -0700

    Added a CrossOrigin annotation to the TranslationRestController for testing with a front-end application

--- src/main/java/com/example/translateapp/controller/TranslationRestController.java
+++ src/main/java/com/example/translateapp/controller/TranslationRestController.java
@@ -8,6 +8,7 @@ import com.example.translateapp.model.TranslationResponseContainer;
 import com.example.translateapp.repository.LanguageRepository;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 public class TranslationRestController {
 
     private final RestTemplate template;
